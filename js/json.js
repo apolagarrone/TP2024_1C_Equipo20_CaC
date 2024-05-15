c4037472e74ab7beacf59153cd6ba53b56c45ba2
Agustina <agustina.guantay1999@gmail.com>
2024-05-15 20:15:50 -0300

    cambios json

--- js/json.js
+++ js/json.js
@@ -1,2 +1,9 @@
 
+fetch("https://apolagarrone.github.io/TP2024_1C_Equipo20_CaC/datos.json")
+.then(response => response.json())
+.then(data => {
+    // Procesamiento de la info que llega de la API
+    console.log(data);
+    })
+.catch(error => console.log("Ocurrió un error!"));
 
