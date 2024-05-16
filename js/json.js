83d21fa64cc992b38b6da2cdef333d0ae60ad076
Agustina <agustina.guantay1999@gmail.com>
2024-05-15 22:53:34 -0300

    json

--- js/json.js
+++ js/json.js
@@ -3,24 +3,36 @@ let personaCopia=persona.cloneNode(true);
 
 let contenedor=document.querySelector("main");
 persona.remove()
+ let botonAgregar=document.querySelector("#Agregar");
+ botonAgregar.addEventListener("click", function(){
+  AgregarPersona();
+ });
+let indexPersona=0;
+ function AgregarPersona(){
+    fetch("https://apolagarrone.github.io/TP2024_1C_Equipo20_CaC/datos.json")
+    .then(response => response.json())
+    .then(data => {
+        // Procesamiento de la info que llega de la API
+        let destino = data[0].Destinos[indexPersona];
+        
+        let nuevaPersona = personaCopia.cloneNode(true);
+        nuevaPersona.querySelector("#Foto").src = destino.image;
+        nuevaPersona.querySelector("#Foto").alt = "fot CV";
+        nuevaPersona.querySelector("#Nombre").innerHTML = destino.Nombre;
+        nuevaPersona.querySelector("#Origen").innerHTML = destino.Servicio;
+        nuevaPersona.querySelector("#Descripcion").innerHTML = destino.Descripcion;
 
+        contenedor.appendChild(nuevaPersona);
 
-fetch("https://apolagarrone.github.io/TP2024_1C_Equipo20_CaC/datos.json")
-.then(response => response.json())
-.then(data => {
-    // Procesamiento de la info que llega de la API
-   let nuevaPersona=personaCopia.cloneNode(true);
+        // Incrementar el índice para la próxima persona
+        indexPersona++;
 
-   nuevaPersona.querySelector("#Foto").src=data[0].Destinos[1].image;
-   nuevaPersona.querySelector("#Foto").alt="fot CV";
-   nuevaPersona.querySelector("#Nombre").innerHTML=data[0].Destinos[0].Nombre;
-   nuevaPersona.querySelector("#Origen").innerHTML=data[0].Destinos[0].Origen;
-   nuevaPersona.querySelector("#Descripcion").innerHTML=data[0].Destinos[0].Descripcion;
-   
-   contenedor.appendChild(nuevaPersona);
-
-
-
+        
+        if (indexPersona >= data[0].Destinos.length) {
+            botonAgregar.disabled = true; // Desactivar el botón "Agregar" si ya no hay más personas por agregar
+        }
     })
-.catch(error => console.log("Ocurrió un error!"));
+    .catch(error => console.log("Ocurrió un error! " + error));
+}
+   
 
