75d34ebb26de4595b48b701a2217e0778e4e80da
Agustina <agustina.guantay1999@gmail.com>
2024-05-15 21:35:51 -0300

    img

--- js/json.js
+++ js/json.js
@@ -13,9 +13,9 @@ fetch("https://apolagarrone.github.io/TP2024_1C_Equipo20_CaC/datos.json")
 
    nuevaPersona.querySelector("#Foto").src=data[0].Destinos[1].image;
    nuevaPersona.querySelector("#Foto").alt="fot CV";
-   nuevaPersona.querySelector("#Nombre").innerHTML=data[0].Destinos[1].Nombre;
-   nuevaPersona.querySelector("#Origen").innerHTML=data[0].Destinos[1].Origen;
-   nuevaPersona.querySelector("#Descripcion").innerHTML=data[0].Destinos[1].Descripcion;
+   nuevaPersona.querySelector("#Nombre").innerHTML=data[0].Destinos[0].Nombre;
+   nuevaPersona.querySelector("#Origen").innerHTML=data[0].Destinos[0].Origen;
+   nuevaPersona.querySelector("#Descripcion").innerHTML=data[0].Destinos[0].Descripcion;
    
    contenedor.appendChild(nuevaPersona);
 
